b8ca5510ce2d8b0efcbdecd44925d25231ada6c0
romeromfm <9dhxvdpd0>
2015-03-06 13:00:34 -0300

    Correction on profile; Unit Tests for LogisticsMap creation and BestRoute calculation;

--- src/test/java/challange/walmart/DeliveryWebServiceTest.java
+++ src/test/java/challange/walmart/DeliveryWebServiceTest.java
@@ -5,6 +5,7 @@ import challange.walmart.internals.util.TestUtil;
 import challange.walmart.model.DeliveryPath;
 import challange.walmart.model.DeliveryPoint;
 import challange.walmart.service.LogisticsService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(locations = {
 	"classpath:WEB-INF/application-context.xml",
 	"classpath:WEB-INF/database/database-context.xml" })
-@ActiveProfiles(profiles = { "eclipselink" })
+@ActiveProfiles(profiles = { "hibernate" })
 @Transactional(propagation = Propagation.REQUIRED)
 @TransactionConfiguration(defaultRollback = true)
 public class DeliveryWebServiceTest {
@@ -120,5 +121,52 @@ public class DeliveryWebServiceTest {
 		verifyNoMoreInteractions(logisticsServiceMock);
 	}
 
+	@Test
+	public void assertLogisticsMapExists() throws Exception {
+		final String POINT_A = "A";
+		final String POINT_B = "B";
+		final String MAP_NAME = "A-B";
+		final Double DISTANCE = 5.0;
 
+		this.logisticsServiceMock.createLogisticsMap(
+			POINT_A, POINT_B, MAP_NAME, DISTANCE
+		);
+
+		DeliveryPoint deliveryPointA = this.logisticsServiceMock.findDeliveryPointByName(POINT_A);
+		Assert.assertNotNull(deliveryPointA);
+
+
+		DeliveryPoint deliveryPointB = this.logisticsServiceMock.findDeliveryPointByName(POINT_B);
+		Assert.assertNotNull(deliveryPointB);
+	}
+
+	@Test
+	public void assetLogisticsMapsExistForFindingBestRoute() throws Exception {
+		final String POINT_A = "A";
+		final String POINT_B = "B";
+		final String MAP_NAME = "A-B";
+		final Double DISTANCE = 5.0;
+		final Double AUTONOMY = 10D;
+		final Double FUEL_PRICE = 3D;
+
+		this.logisticsServiceMock.createLogisticsMap(
+			POINT_A, POINT_B, MAP_NAME, DISTANCE
+		);
+
+		DeliveryPoint deliveryPointA = this.logisticsServiceMock.findDeliveryPointByName(POINT_A);
+		Assert.assertNotNull(deliveryPointA);
+
+		DeliveryPoint deliveryPointB = this.logisticsServiceMock.findDeliveryPointByName(POINT_B);
+		Assert.assertNotNull(deliveryPointB);
+
+		BestRoutDTO bestRoutDTO = this.logisticsServiceMock.calculateBestRoute(
+			deliveryPointA,
+			deliveryPointB,
+			AUTONOMY,
+			FUEL_PRICE);
+
+		System.out.println(bestRoutDTO);
+
+		Assert.assertNotNull(bestRoutDTO);
+	}
 }
