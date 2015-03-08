0649efd3778e03b4e15c5411ff6c76e2efa38379
romeromfm <9dhxvdpd0>
2015-03-08 20:44:20 -0300

    Tests for: calculating best route, map creation, and DeliveryPoints creation

--- src/test/java/challange/walmart/DeliveryLocalTests.java
+++ src/test/java/challange/walmart/DeliveryLocalTests.java
@@ -1,6 +1,7 @@
 package challange.walmart;
 
 import challange.walmart.dto.BestRoutDTO;
+import challange.walmart.model.DeliveryPath;
 import challange.walmart.model.DeliveryPoint;
 import challange.walmart.service.LogisticsService;
 import org.junit.Assert;
@@ -30,7 +31,7 @@ public class DeliveryLocalTests {
 	private LogisticsService logisticsServiceMock;
 
 	@Test
-	public void assertLogisticsMapExists() throws Exception {
+	public void assertDeliveryPointsExistsAfterCreatingLogisticsMap() throws Exception {
 		final String POINT_A = "A";
 		final String POINT_B = "B";
 		final String MAP_NAME = "A-B";
@@ -42,39 +43,69 @@ public class DeliveryLocalTests {
 
 		DeliveryPoint deliveryPointA = this.logisticsServiceMock.findDeliveryPointByName(POINT_A);
 		Assert.assertNotNull(deliveryPointA);
+        Assert.assertEquals(deliveryPointA.getName(), POINT_A);
 
 
 		DeliveryPoint deliveryPointB = this.logisticsServiceMock.findDeliveryPointByName(POINT_B);
 		Assert.assertNotNull(deliveryPointB);
+        Assert.assertEquals(deliveryPointB.getName(), POINT_B);
 	}
 
-	@Test
-	public void assetLogisticsMapsExistForFindingBestRoute() throws Exception {
-		final String POINT_A = "A";
-		final String POINT_B = "B";
-		final String MAP_NAME = "A-B";
-		final Double DISTANCE = 5.0;
-		final Double AUTONOMY = 10D;
-		final Double FUEL_PRICE = 3D;
+    @Test
+    public void assertLogisticsMapCreation() {
+        final String MAP_NAME = "AB";
+        final String POINT_A = "A";
+        final String POINT_B = "B";
+        final Double DISTANCE = 10D;
 
-		this.logisticsServiceMock.createLogisticsMap(
-			POINT_A, POINT_B, MAP_NAME, DISTANCE
-		);
+        this.logisticsServiceMock.createLogisticsMap(MAP_NAME, POINT_A, POINT_B, DISTANCE);
 
-		DeliveryPoint deliveryPointA = this.logisticsServiceMock.findDeliveryPointByName(POINT_A);
-		Assert.assertNotNull(deliveryPointA);
+        DeliveryPath logisticsMap = this.logisticsServiceMock.findDeliveryPathByName("AB");
 
-		DeliveryPoint deliveryPointB = this.logisticsServiceMock.findDeliveryPointByName(POINT_B);
-		Assert.assertNotNull(deliveryPointB);
+        Assert.assertNotNull(logisticsMap);
+        Assert.assertEquals(logisticsMap.getName(), "AB");
+    }
 
-		BestRoutDTO bestRoutDTO = this.logisticsServiceMock.calculateBestRoute(
-			deliveryPointA,
-			deliveryPointB,
-			AUTONOMY,
-			FUEL_PRICE);
+    @Test
+    public void assertBestRoutCalculation() {
+        DeliveryPath deliveryPath1 = new DeliveryPath();
+        deliveryPath1.setId(1L);
+        deliveryPath1.setName("AB");
+        deliveryPath1.setDistance(10.0);
 
-		System.out.println(bestRoutDTO);
+        DeliveryPath deliveryPath2 = new DeliveryPath();
+        deliveryPath2.setId(2L);
+        deliveryPath2.setName("CD");
+        deliveryPath2.setDistance(15.0);
 
-		Assert.assertNotNull(bestRoutDTO);
-	}
+        DeliveryPath deliveryPath3 = new DeliveryPath();
+        deliveryPath3.setId(3L);
+        deliveryPath3.setName("AC");
+        deliveryPath3.setDistance(20.0);
+
+        DeliveryPath deliveryPath4 = new DeliveryPath();
+        deliveryPath4.setId(4L);
+        deliveryPath4.setName("CD");
+        deliveryPath4.setDistance(30.0);
+
+        DeliveryPath deliveryPath5 = new DeliveryPath();
+        deliveryPath5.setId(5L);
+        deliveryPath5.setName("BE");
+        deliveryPath5.setDistance(50.0);
+
+        DeliveryPath deliveryPath6 = new DeliveryPath();
+        deliveryPath6.setId(6L);
+        deliveryPath6.setName("DE");
+        deliveryPath6.setDistance(30.0);
+
+        DeliveryPoint originPoint = this.logisticsServiceMock.findDeliveryPointByName("A");
+        DeliveryPoint destinyPoint = this.logisticsServiceMock.findDeliveryPointByName("B");
+        Double autonomy = 10D;
+        Double fuelPrice = 3D;
+
+        BestRoutDTO bestRoutDTO = this.logisticsServiceMock.calculateBestRoute(originPoint, destinyPoint, autonomy, fuelPrice);
+
+        Assert.assertNotNull(bestRoutDTO);
+        System.out.println(" >>> Best Route: " + bestRoutDTO);
+    }
 }
